316dcbe100ae76d91ab2a7135f94467875cb0e8c
Anton Menchuk <menchukanton@gmail.com>
2019-08-25 14:14:21 +0300

    Studying commit

--- src/test/java/SearchOffroadTests.java
+++ src/test/java/SearchOffroadTests.java
@@ -1,2 +1,10 @@
+import org.testng.annotations.Test;
+
 public class SearchOffroadTests extends MainTests {
+
+    @Test
+    public void searchTyresOffroads() throws InterruptedException {
+        mainPage.open();
+        mainPage.cookiesButton();
+    }
 }
--- src/test/java/SearchPKWTests.java
+++ src/test/java/SearchPKWTests.java
@@ -3,6 +3,14 @@ import org.testng.annotations.Test;
 
 public class SearchPKWTests extends MainTests {
 
+    @DataProvider(name = "dataForPKW")
+    public Object[][] dataForPKW() {
+        return new Object[][] {
+                {"winter", "https://reifen.pkwteile.de/reifen-search?Season=winter&Width=255&CrossSections=65&Size=16"},
+                {"sommer", "https://reifen.pkwteile.de/reifen-search?Season=winter&Width=255&CrossSections=65&Size=16"},
+        };
+    }
+
     @Test(dataProvider = "dataForPKW")
     public void searchTyres(String season,String wight, String expectedURL) throws InterruptedException {
         mainPage.open();
@@ -16,11 +24,5 @@ public class SearchPKWTests extends MainTests {
         searchFormCar.checkPageURL(expectedURL);
     }
 
-    @DataProvider(name = "dataForPKW")
-    public Object[][] dataForPKW() {
-        return new Object[][] {
-                {"winter", "https://reifen.pkwteile.de/reifen-search?Season=winter&Width=255&CrossSections=65&Size=16"},
-                {"sommer", "https://reifen.pkwteile.de/reifen-search?Season=winter&Width=255&CrossSections=65&Size=16"},
-        };
-    }
+
 }
